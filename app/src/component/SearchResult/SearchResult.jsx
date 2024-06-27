fe8666177e3b433a74f5fba1f430043062587dac
Yash <yash123456789.yg@gmail.com>
2024-06-27 09:32:41 +0530

    Initial commit

---
+++ app/src/component/SearchResult/SearchResult.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { BASE_URL, Button } from "../../App";
+
+
+const SearchResult = ({data}) => {
+  return (
+    <Foodcontainer>
+    <FoodCards>
+        {
+            data?.map(({name, image, text, price}) => <FoodCard
+                key={name}>
+                    <div className="food_image">
+                        <img src={BASE_URL + image} />
+                    </div>
+                    <div className="food_info">
+                        <div className="info">
+                            <h3>{name}</h3>
+                            <p>{text}</p>
+                        </div>
+                    </div>
+                    <Button>${price.toFixed(2)}</Button>
+            </FoodCard> )
+        }   
+    </FoodCards>
+  </Foodcontainer>
+  )
+}
+
+export default SearchResult;
+
+const Foodcontainer = styled.section`
+background-image: url("/images/bg.png");
+background-size: cover;
+height: calc(100vh - 210px);
+`;
+
+const FoodCards = styled.div`
+
+`;
+
+const FoodCard = styled.div`
+
+`;
